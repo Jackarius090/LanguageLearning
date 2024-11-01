09b89bcdd7ef2c0d5a48e645735e81dbe3a29ff8
Jack Dickinson <jackdickinson090@hotmail.com>
2024-11-01 13:47:50 +0800

    add dark mode button, remove excess css, change main text colour

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+// TODO Add dark mode
+// TODO Add GPT function
+
 import { useState } from 'react'
-import './App.css'
 import { Textarea } from "@/components/ui/textarea"
 import NavBar from './components/NavBar'
 import HighlightedText from '@/components/HighlightedText'
+import { Switch } from "@/components/ui/switch"
 
 
 function App() {
@@ -11,14 +14,19 @@ function App() {
   const [text, setText] = useState('')
 
   return (
-    
     <div className="size-full">
       <div className="m-6 ml-48">
         <NavBar className="flex flex-wrap" setText={setText}/>
       </div>
       <div className="w-full mb-8 flex items-center">
-        <img src='public/favicon-512x512.png' className='w-16 sm:w-20 md:w-24 lg:w-28 xl:w-32 2xl:w-36 mx-10'></img>
+        <img src='/favicon-512x512.png' className='w-16 sm:w-20 md:w-24 lg:w-28 xl:w-32 2xl:w-36 mx-10'></img>
         <h2 className="w-9/12 text-2xl md:text-3xl lg:text-4xl font-bold">Language Learning App</h2>
+        <div className="flex items-center space-x-2">
+          <label htmlFor="dark-mode">
+            Dark mode
+            <Switch id="dark-mode" />
+          </label>
+        </div>
       </div>
       <div className="flex pt-8 mb-4 h-screen border-2">
         <div className='flex size-full p-5'>
