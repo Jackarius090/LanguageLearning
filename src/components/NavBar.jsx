701bcddd0e6d3bb7c19dfe3f655f71eb2415f06b
Jack Dickinson <jackdickinson090@hotmail.com>
2024-11-06 14:31:11 +0800

    add eslint config, tidy files

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,26 +1,22 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import { cn } from "@/lib/utils"
 import {
   NavigationMenu,
   NavigationMenuItem,
   NavigationMenuList,
   NavigationMenuTrigger,
   NavigationMenuContent,
-  NavigationMenuLink,
-} from "@/components/ui/navigation-menu"
+} from "@/components/ui/navigation-menu";
 
-const NavBar = ({setText}) => {
+const NavBar = ({ setText }) => {
+  const handleDanishClick = () => {
+    const placeholderText =
+      "Sønderborg er en lille by i Sønderjylland. Den er ikke særlig stor, men hyggelig. I en gade i Sønderborg står der et stort, gammelt, rødt hus. Der bor Morten sammen med sin gode ven Thomas. De to er flyttet ind i lejligheden i august, lige før det nye semester begyndte. I deres hus bor der også nogle andre personer. På den første etage bor der en matematiklærer med sin kone og deres to småbørn. Påanden etage bor en gammel dame, der engang har arbejdet som frisør. Hun er meget sød og har tit besøg af hendes børnebørn. De kommer gerne forbi hos hende for at høre spændende historier fra den tid mormor var ung og for at fånoget af hendes lækre kage. Morten og Thomas har en hyggelig lejlighed. I køkkenet stå r der et bord og fire stole. Der sidder de tit sammen med deres venner og spiser. Morten er nemlig meget godt til at lave mad. Thomas kan det ikke så godt, så vasker han alltid op.";
+    setText(placeholderText);
+  };
 
-    const handleDanishClick = () => {
-        const placeholderText = "Sønderborg er en lille by i Sønderjylland. Den er ikke særlig stor, men hyggelig. I en gade i Sønderborg står der et stort, gammelt, rødt hus. Der bor Morten sammen med sin gode ven Thomas. De to er flyttet ind i lejligheden i august, lige før det nye semester begyndte. I deres hus bor der også nogle andre personer. På den første etage bor der en matematiklærer med sin kone og deres to småbørn. Påanden etage bor en gammel dame, der engang har arbejdet som frisør. Hun er meget sød og har tit besøg af hendes børnebørn. De kommer gerne forbi hos hende for at høre spændende historier fra den tid mormor var ung og for at fånoget af hendes lækre kage. Morten og Thomas har en hyggelig lejlighed. I køkkenet stå r der et bord og fire stole. Der sidder de tit sammen med deres venner og spiser. Morten er nemlig meget godt til at lave mad. Thomas kan det ikke så godt, så vasker han alltid op."
-        setText(placeholderText)
-        }
-
-    const handleChineseClick = () => {
-        const placeholderText = 
-`Drunkenness (2.4) - Hou Baolin (1913-1993) 
+  const handleChineseClick = () => {
+    const placeholderText = `Drunkenness (2.4) - Hou Baolin (1913-1993) 
 醉酒(片段)
 侯寶林 郭啟如
 侯：真喝醉的人，怕別人說他喝醉了。
@@ -47,93 +43,106 @@ const NavBar = ({setText}) => {
 Hou Baolin (1913-1993) was one of the most renowned practitioners of xiangsheng comic dialog, or crosstalk, in the 20th century. The modern form of crosstalk dates from the mid-19th century, and along with other forms of quyi, or folk vocal art, experienced a period of great popularity after the founding of the People's Republic. This dialog is extracted from one of the best-known of those performed by Hou Baolin and his partner Guo Qiru in the republic's early years. 
 
 Source: https://public.websites.umich.edu/~dporter/sampler/sampler.html
-`
-        setText(placeholderText)
-        }
-        
-    
+`;
+    setText(placeholderText);
+  };
+
   return (
     <div>
-        <NavigationMenu>
+      <NavigationMenu>
         <NavigationMenuList className="flex justify-center flex-wrap">
-            <NavigationMenuItem>
+          <NavigationMenuItem>
             <NavigationMenuTrigger>Sample texts</NavigationMenuTrigger>
             <NavigationMenuContent>
-                <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
+              <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
                 <li>
-                    <div onClick={handleDanishClick} className="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground">
-                        <div className="text-sm font-medium leading-none">🇩🇰 Danish text</div>
-                        <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
-                            Add a sample Danish text to the text area.
-                        </p>
+                  <div
+                    onClick={handleDanishClick}
+                    className="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
+                  >
+                    <div className="text-sm font-medium leading-none">
+                      🇩🇰 Danish text
                     </div>
+                    <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+                      Add a sample Danish text to the text area.
+                    </p>
+                  </div>
                 </li>
                 <li className="row-span-3">
-                    <div className="flex items-center place-content-center h-full w-full select-none flex-col rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md">
-                        <img className="dark:brightness-75 size-12 mb-5" src="public/favicon-192x192.png" alt="" />
-                        <div className="text-lg font-medium">
-                            Choose a sample text to read!
-                        </div>
+                  <div className="flex items-center place-content-center h-full w-full select-none flex-col rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md">
+                    <img
+                      className="dark:brightness-75 size-12 mb-5"
+                      src="public/favicon-192x192.png"
+                      alt=""
+                    />
+                    <div className="text-lg font-medium">
+                      Choose a sample text to read!
                     </div>
+                  </div>
                 </li>
                 <li>
-                    <div onClick={handleChineseClick} className="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground">
-                        <div className="text-sm font-medium leading-none">Chinese text</div>
-                        <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
-                            Add a sample Traditional Chinese text to the text area.
-                        </p>
+                  <div
+                    onClick={handleChineseClick}
+                    className="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
+                  >
+                    <div className="text-sm font-medium leading-none">
+                      Chinese text
                     </div>
+                    <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+                      Add a sample Traditional Chinese text to the text area.
+                    </p>
+                  </div>
                 </li>
                 <SimpleListItem title="Typography Styles">
-                    Well-designed typography for headings, paragraphs, and more.
+                  Well-designed typography for headings, paragraphs, and more.
                 </SimpleListItem>
-                
-                </ul>
+              </ul>
             </NavigationMenuContent>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
             <NavigationMenuTrigger>Components</NavigationMenuTrigger>
             <NavigationMenuContent>
-                <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
+              <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
                 <SimpleListItem title="Alert Dialog">
-                    A modal dialog that interrupts the user with important content.
+                  A modal dialog that interrupts the user with important
+                  content.
                 </SimpleListItem>
                 <SimpleListItem title="Hover Card">
-                    Preview content available behind a link on hover.
+                  Preview content available behind a link on hover.
                 </SimpleListItem>
                 <SimpleListItem title="Progress">
-                    Displays task completion as a progress bar.
+                  Displays task completion as a progress bar.
                 </SimpleListItem>
                 <SimpleListItem title="Scroll Area">
-                    Visually separates content using scrollable areas.
+                  Visually separates content using scrollable areas.
                 </SimpleListItem>
                 <SimpleListItem title="Tabs">
-                    Organized sections of content displayed one at a time.
+                  Organized sections of content displayed one at a time.
                 </SimpleListItem>
                 <SimpleListItem title="Tooltip">
-                    Displays additional information when an element is focused or hovered.
+                  Displays additional information when an element is focused or
+                  hovered.
                 </SimpleListItem>
-                </ul>
+              </ul>
             </NavigationMenuContent>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
             <NavigationMenuTrigger>Components</NavigationMenuTrigger>
             <NavigationMenuContent>
-                <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
+              <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
                 <SimpleListItem title="Scroll Area">
-                    Visually separates content using scrollable areas.
+                  Visually separates content using scrollable areas.
                 </SimpleListItem>
-                </ul>
+              </ul>
             </NavigationMenuContent>
-            </NavigationMenuItem>
+          </NavigationMenuItem>
         </NavigationMenuList>
-        </NavigationMenu>
+      </NavigationMenu>
     </div>
-  )
-}
+  );
+};
 
 export default NavBar;
-
 
 const SimpleListItem = ({ title, children }) => {
   return (
@@ -145,5 +154,5 @@ const SimpleListItem = ({ title, children }) => {
         </p>
       </div>
     </li>
-  )
-}
+  );
+};
--- src/components/ui/textarea.jsx
+++ src/components/ui/textarea.jsx
@@ -1,69 +1,89 @@
 import * as React from "react";
 import { cn } from "@/lib/utils";
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 
-const Textarea = React.forwardRef(({ className, setHighlightedText, highlightedText, language, setLanguage, setTranslation, text, setText, ...props }, ref) => {
-  const apiKey = import.meta.env.VITE_GOOGLE_CLOUD_API_KEY;;
+const Textarea = React.forwardRef(
+  (
+    {
+      className,
+      setHighlightedText,
+      language,
+      setLanguage,
+      setTranslation,
+      text,
+      setText,
+      ...props
+    },
+    ref
+  ) => {
+    const apiKey = import.meta.env.VITE_GOOGLE_CLOUD_API_KEY;
 
-  const handleMouseUp = () => {
-    const selectedText = window.getSelection().toString();
-    if (selectedText) {
-      setHighlightedText(selectedText);  // Update the highlightedText state
-      translateText(selectedText);       // Pass the selected text for translation
-    }
-  };
-
-  const translateText = async (TexttoTranslate) => {
-    try {
-      const response = await fetch(
-        `https://translation.googleapis.com/language/translate/v2?key=${apiKey}`,
-        {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            q: TexttoTranslate,      // Text to translate
-            target: "en", // Target language code
-          }),
-        }
-      );
-      const data = await response.json();
-      if (data && data.data && data.data.translations) {
-        setTranslation(data.data.translations[0].translatedText);  // Get the translated text
-        setLanguage(data.data.translations[0].detectedSourceLanguage)
-        console.log(JSON.stringify(data, null, 2));
-      } else {
-        setTranslation('Error translating text');
+    const handleMouseUp = () => {
+      const selectedText = window.getSelection().toString();
+      if (selectedText) {
+        setHighlightedText(selectedText); // Update the highlightedText state
+        translateText(selectedText); // Pass the selected text for translation
       }
-    } catch (error) {
-      console.error('Error with translation API:', error);
-      setTranslation('Failed to translate');
-    }
-  };
+    };
 
-// const handleClearClick = () => setText("")
+    const translateText = async (TexttoTranslate) => {
+      try {
+        const response = await fetch(
+          `https://translation.googleapis.com/language/translate/v2?key=${apiKey}`,
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              q: TexttoTranslate, // Text to translate
+              target: "en", // Target language code
+            }),
+          }
+        );
+        const data = await response.json();
+        if (data && data.data && data.data.translations) {
+          setTranslation(data.data.translations[0].translatedText); // Get the translated text
+          setLanguage(data.data.translations[0].detectedSourceLanguage);
+          console.log(JSON.stringify(data, null, 2));
+        } else {
+          setTranslation("Error translating text");
+        }
+      } catch (error) {
+        console.error("Error with translation API:", error);
+        setTranslation("Failed to translate");
+      }
+    };
 
-  return (
-    <div className="size-full flex flex-col place-items-center  max-w-74ch">
-      <Button className="m-3 w-4/5" variant="outline" onClick={() => setText("")}>Clear textbox</Button>
-      <p>Language: {language}</p>
-      <textarea
-        spellCheck={false}
-        onMouseUp={handleMouseUp}
-        value={text}
-        onChange={e => setText(e.target.value)}
-        placeholder="Add text here..."
-        className={cn(
-          "text-xs sm:text-sm md:text-md lg:text-lg size-full rounded-md border border-input bg-background px-3 py-2 ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
-          className
-        )}
-        ref={ref}
-        {...props}
-      />
-    </div>
-  );
-});
+    // const handleClearClick = () => setText("")
+
+    return (
+      <div className="size-full flex flex-col place-items-center  max-w-74ch">
+        <Button
+          className="m-3 w-4/5"
+          variant="outline"
+          onClick={() => setText("")}
+        >
+          Clear textbox
+        </Button>
+        <p>Language: {language}</p>
+        <textarea
+          spellCheck={false}
+          onMouseUp={handleMouseUp}
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          placeholder="Add text here..."
+          className={cn(
+            "text-xs sm:text-sm md:text-md lg:text-lg size-full rounded-md border border-input bg-background px-3 py-2 ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+            className
+          )}
+          ref={ref}
+          {...props}
+        />
+      </div>
+    );
+  }
+);
 
 Textarea.displayName = "Textarea";
 
