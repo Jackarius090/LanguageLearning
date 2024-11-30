d17dfd506bc8231b2323e6b374f1e900a42d58ec
Jack Dickinson <jackdickinson090@hotmail.com>
2024-11-30 18:19:00 +0100

    add popup warning when using touch screen device

--- LLClient/src/App.jsx
+++ LLClient/src/App.jsx
@@ -6,13 +6,35 @@
 // TODO change colors of themes
 // TODO pronunciation sound clips
 // TODO Add user login - https://workos.com/user-management
+// TODO Add ANKI codes
 
 import NavBar from "./components/NavBar";
 import Banner from "./components/Banner";
 import TextWindow from "./components/TextWindow";
 import { Toaster } from "@/components/ui/toaster";
+import { useToast } from "@/hooks/use-toast";
+import { useEffect } from "react";
 
 function App() {
+  const { toast } = useToast();
+
+  useEffect(() => {
+    const isTouchDevice =
+      "ontouchstart" in window ||
+      navigator.maxTouchPoints > 0 ||
+      navigator.msMaxTouchPoints > 0;
+
+    if (isTouchDevice) {
+      toast({
+        variant: "destructive",
+        title: "Touch Screen Device Detected",
+        description:
+          "This app requires a mouse to highlight text. Please use a computer to try out this website.",
+        duration: Infinity,
+      });
+    }
+  }, [toast]);
+
   return (
     <div className="size-full min-h-screen text-primary">
       <NavBar className="flex flex-wrap" />
